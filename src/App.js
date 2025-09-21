c5a3aeb9c145ca69137bbe5ac38f217455aaced9
Эльдар <zainullin182@gmail.com>
2025-09-21 12:35:34 +0300

    modal is added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import React from 'react';
+import {useState} from 'react';
 import './index.scss';
+import Modal from './Components/Modal/Modal';
 
 function App() {
+  const [isOpen, setOpen] = useState(false)
+  const togglePopUp = () => setOpen(!isOpen)
   return (
     <div className="App">
-      <button className="open-modal-btn">✨ Открыть окно</button>
-      {/* <div className="overlay">
-        <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200">
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-        </div>
-      </div> */}
+      <button onClick={togglePopUp} className="open-modal-btn">✨ Открыть окно</button>
+      <Modal isOpen={isOpen} onClose={togglePopUp} />
     </div>
   );
 }
